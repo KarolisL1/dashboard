7c7c3187b33ffc0bb7f693b57e442e65ed4ae7f4
karolisliepa <Karolis.Liepa@gmail.com>
2022-06-02 17:07:06 -0400

    app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+    const activeMenu = false;
+
     return (
         <div>
             <BrowserRouter>
@@ -21,15 +23,42 @@ const App = () => {
                     </TooltipComponent>
                 </div>
                 {activeMenu ? (
-                    <div>
+                    <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
                         Sidebar
                     </div>
                 ) : (
-                    <div>
-                        Sidebar
+                    <div className='w-0 dark:bg-secondary-dark-bg'>
+                        Sidebar 
                     </div>
                 )}
-            </div>
+                <div className={
+                    `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                }>
+                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                        NavBar
+                    </div>
+                </div>
+
+                <div>
+                    <Routes>
+                        {/* Dashboard */}
+                        <Route path='/' element="EcoMerce" />
+                        <Route path='/ecomerce' element="EcoMerce" />
+
+                        {/* Pages */}
+                        <Route path='/orders' element="Orders" />
+                        <Route path='/employees' element="Employees" />
+                        <Route path='/customers' element="Customers" />
+
+                        {/* Apps */}
+                        <Route path='/kanban' element="Kanban" />
+                        <Route path='/editor' element="Editor" />
+                        <Route path='/calendar' element="Calendar" />
+                        <Route path='/color-picker' element="ColorPicker" />
+                    </Routes>
+                </div>
+
+            </div> 
             </BrowserRouter>
         </div>
     )
