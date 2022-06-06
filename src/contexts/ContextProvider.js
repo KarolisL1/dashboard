5016d7bcba1cbf494573febf0a074a606bfb6c5d
karolisliepa <Karolis.Liepa@gmail.com>
2022-06-06 18:58:37 -0400

    ecommerce

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -13,12 +13,10 @@ export const ContextProvider = ({ children }) => {
 
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
 
-    const handleClick = (clicked) => {
-        setIsClicked({ ...initialState, [clicked] : true})
-    }
-
-
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked] : true});
+    
     return (
         <StateContext.Provider 
             value={{ 
@@ -26,11 +24,14 @@ export const ContextProvider = ({ children }) => {
                 setActiveMenu,
                 isClicked,
                 setIsClicked,
-                handleClick
+                handleClick,
+                screenSize,
+                setScreenSize
             }}
         >
             {children}
         </StateContext.Provider>
-    )}
+    );
+};
 
 export const useStateContext = () => useContext(StateContext);
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BsCurrencyDollart } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+
+import { Stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparklineAreaData, ecomPieChartDat } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
+    <div className="mt-12">
+      <div className="flex flew-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$45,595.78</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
