2e2ef4da4f513f0773c52303c83d6cd7c5199d41
karolisliepa <Karolis.Liepa@gmail.com>
2022-06-13 16:59:21 -0400

    pie

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 
-const Pie = () => {
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const Pie = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pie</div>
+    <AccumulationChartComponent id={id}>
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective dataSource={data} xName='x' yName='y' radius='100%'>
+        </AccumulationSeriesDirective>
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
   )
 }
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -105,9 +105,7 @@ const Ecommerce = () => {
                 />
               </div>
             </div>
-            <div>
-              <Stacked width="320px" height="360px"/>
-            </div>
+            <div><Stacked width="320px" height="360px"/></div>
           </div>
         </div>
       </div>
